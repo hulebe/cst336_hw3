2f563ed8745690928bdfe9178a3ed274c4d08606
EC2 Default User <ec2-user@ip-172-31-24-95.us-west-1.compute.internal>
2020-11-18 11:54:08 +0000

    update

--- index.php
+++ index.php
@@ -8,34 +8,73 @@
     </head>
     
     <body>
-        <h1>Weather</h1>    
-        <hr>
-        Check your local weather! <br><br>
+        <h1>Weather</h1>
+        <hr> 
+        
+        <p><h2>Check your local weather!</h2></p> <br><br>
         <form id="weatherForm">
-            Zip Code:   <input type="text" id="zip" name="zip">
-                        <span id="invalidZip"></span>
-            <input type="submit" id="search" value="Search"> <br>
-            
-            <span id="currentWeather"></span>
+            <input type="text" id="zip" name="zip" placeholder="Enter zip code">
+            <input type="button" onclick="showMessage()" value="Search"/>
         </form>
         
+        <span id="invalidZip"></span> <br><br>
+        <div>
+            <span class="temp" id="city"></span><br><br>
+            <span class="temp" id="currentWeather"></span><br><br>
+            <span class="temp" id="description"></span><br><br>
+            <span class="temp" id="feels_like"></span><br><br>
+        </div>
+        
+        
+        
         <script>
-
+            
+            var zipCodeAvailable = true;
              
-            $("#zip").on("click",async function() {
-                
+            $("#zip").on("change",async function() {
                 let zipCode = $("#zip").val();
-                let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=62ff9c71ee612bbc61238440ffeda18c`;
+                let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=62ff9c71ee612bbc61238440ffeda18c&units=imperial`;
                 let response = await fetch(url);
                 let data = await response.json();
                 
                 console.log(data);
-                
-                $("#currentWeather").html("Current Weather:" + data.main.temp);
+                if (data.cod != 404){
+                    $("#city").html("City of " + data.name);
+                    $("#currentWeather").html("Current Weather: " + data.main.temp);
+                    $("#description").html(data.weather[0].description + "<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png'>");
+                    $("#feels_like").html("Feels like: " + data.main.feels_like);
+                    $("#invalidZip").html("");
+                    zipCodeAvailable = true;
+                } else {
+                    $("#invalidZip").html("Invalid Zip Code");
+                    $("#invalidZip").css("color", "red");
+                    zipCodeAvailable = false;
+                }
 
             });
+            
+          //  $("#weatherForm").on("submit", function(event){ 
+                 $("#button").on("submit", function(event){ 
+                if (!isFormValid()) {
+                    event.preventDefault();
+                }
+ 
+            });
+            
+            function isFormValid() {
+                var isValid = true;
+                if (!zipCodeAvailable) {
+                    isValid = false;
+                 }
+                 return isValid;
+            }
 
         </script>
+        
+        <footer>
+            <hr>
+            CST336: Internet Programming HW 3. 2020&copy; <br>
+        </footer>
     </body>
     
 </html>
